ebdfff4cdcec6571e3f229ed49ab2be70567f093
TheDariusz <thedariusz@gmail.com>
2021-08-31 00:07:49 +0200

    the second version of final hibernate model

--- src/main/java/com/thedariusz/warnme/repository/MeteoAlertCategoryRepository.java
+++ src/main/java/com/thedariusz/warnme/repository/MeteoAlertCategoryRepository.java
@@ -4,15 +4,10 @@ import com.thedariusz.warnme.repository.entity.MeteoAlertCategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 @Repository
 public interface MeteoAlertCategoryRepository extends JpaRepository<MeteoAlertCategoryEntity, Long> {
-    List<MeteoAlertCategoryEntity> findMeteoAlertCategoryEntitiesByName(String name);
-    Set<MeteoAlertCategoryEntity> findMeteoAlertCategoryEntitiesByNameIn(Set<String> names);
-    Set<MeteoAlertCategoryEntity> findByNameIn(Set<String> names);
     Optional<MeteoAlertCategoryEntity> findMeteoAlertCategoryEntityByName(String name);
 
 }
